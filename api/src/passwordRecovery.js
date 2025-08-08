5ad62d6ce49e127ef2117159acdb36e87aabe9d8
Nicholas Borba <nicholasborba@gmail.com>
2025-08-08 13:48:19 -0300

    adding more cypress tests

--- api/src/passwordRecovery.js
+++ api/src/passwordRecovery.js
@@ -1,11 +1,12 @@
-const { getUser } = require('./users');
+const { getUser, findUserByUsernameOrEmail, updateUser } = require('./users');
 
 function recoverPassword(req, res) {
-  const { username } = req.body;
-  const user = getUser(username);
+  const { email } = req.body;
+  const user = findUserByUsernameOrEmail(email);
   if (!user) {
     return res.status(404).json({ message: 'User not found' });
   }
+  updateUser(user.username, { failedAttempts: 0, blocked: false });
   // Simulate sending recovery email
   return res.status(200).json({ message: `Password recovery instructions sent to ${user.email}` });
 }
